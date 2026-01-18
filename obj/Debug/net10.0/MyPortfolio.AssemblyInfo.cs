8cee25a41c01d1474ee8bc6bbb57ce074d1adce1
Þorri Bjarnason <123247777+GiantMushy@users.noreply.github.com>
2026-01-18 11:35:54 +0000

    uploading images to the repo

--- obj/Debug/net10.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c53fa97ca58aa6ffafc62dda1cd6476dec67a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4141882cc9eb6266b4741c162916d53c5940e778")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
